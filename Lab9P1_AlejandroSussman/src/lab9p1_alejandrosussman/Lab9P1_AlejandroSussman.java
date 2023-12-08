ae6fa95d27464a0ee0b3f469871accbdea42b845
gsussman05 <147879065+gsussman05@users.noreply.github.com>
2023-12-08 16:49:51 -0600

    Add files via upload

---
+++ Lab9P1_AlejandroSussman/src/lab9p1_alejandrosussman/Lab9P1_AlejandroSussman.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab9p1_alejandrosussman;
+//Fila 2 Asiento 4
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Lab9P1_AlejandroSussman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Gusanito_GUI gui=new Gusanito_GUI();
+        gui.setVisible(true);
+    }
+    
+}
